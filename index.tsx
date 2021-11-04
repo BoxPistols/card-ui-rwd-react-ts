15984bf339fda2abd9de87194c2e3dd7d29ff833
BoxPistols <ito.atsu.mail@gmail.com>
2021-11-05 05:14:19 +0900

    2 files modified, 1 added, 1 deleted

--- index.tsx
+++ index.tsx
@@ -1,31 +1,15 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
-import './style.css';
+import './style.scss';
 
-interface AppProps { }
-interface AppState {
-  name: string;
-}
-
-class App extends Component<AppProps, AppState> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'React'
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
-    );
-  }
-}
+const App: React.VFC = () => {
+  return (
+    <div>
+      <Hello />
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+};
 
 render(<App />, document.getElementById('root'));
