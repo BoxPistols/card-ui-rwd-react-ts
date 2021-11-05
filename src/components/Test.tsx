4d2b1b23d6b9ae4099806235d67bab88f966f786
BoxPistols <ito.atsu.mail@gmail.com>
2021-11-06 05:53:00 +0900

    1 file modified

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Data from '../data.json';
-
+import '../style/card.scss';
 // type Props = {
 //   text: string;
 // };
@@ -13,20 +13,6 @@ import Data from '../data.json';
 //     last_name: 'Bluth',
 //     avatar: 'https://reqres.in/img/faces/1-image.jpg',
 //   },
-//   {
-//     id: 2,
-//     email: 'janet.weaver@reqres.in',
-//     first_name: 'Janet',
-//     last_name: 'Weaver',
-//     avatar: 'https://reqres.in/img/faces/2-image.jpg',
-//   },
-//   {
-//     id: 3,
-//     email: 'emma.wong@reqres.in',
-//     first_name: 'Emma',
-//     last_name: 'Wong',
-//     avatar: 'https://reqres.in/img/faces/3-image.jpg',
-//   },
 // ];
 
 const Datas = Data.data;
@@ -34,15 +20,20 @@ const Datas = Data.data;
 export default ({}) => (
   <React.Fragment>
     <h2>some magic happen!</h2>
-    {/* <div> */}
-    {/* <ul> */}
-    {Datas.map((user) => (
-      <li key={user.id} className="card--list">
-        {user.first_name}: {user.email}
-        <img src={user.avatar} alt="avatar" />
-      </li>
-    ))}
-    {/* </ul> */}
-    {/* </div> */}
+    <ul className="card--layout">
+      {Datas.map((user) => (
+        <li key={user.id} className="card--list">
+          <div className="card--head">
+            <div className="card--img">
+              <img src={user.avatar} alt="avatar" />
+            </div>
+          </div>
+          <div className="card--content">
+            <div className="card--name">{user.first_name}</div>
+            <div className="card--email">{user.email}</div>
+          </div>
+        </li>
+      ))}
+    </ul>
   </React.Fragment>
 );
